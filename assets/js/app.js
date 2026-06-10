c5587d3aa99f92cc3ea7b6f8aee5f83488c7e6cc
Jnthzn <sanchezbenja605@gmail.com>
2026-06-10 15:27:39 -0300

    feat: se creó el renderizado de tarjetas de personajes

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,31 +4,52 @@ const contenedorPersonajes = document.getElementById("contenedorPersonajes");
 const mensaje = document.getElementById("mensaje");
 
 async function obtenerPersonajes() {
-    try {
-        const respuesta = await fetch("https://thesimpsonsapi.com/api/characters");
+  try {
+    const respuesta = await fetch("https://thesimpsonsapi.com/api/characters");
 
-        const datos = await respuesta.json();
-        
-        personajes = datos;
-        console.log(personajes);
-        renderizarPersonajes(personajes);
+    const datos = await respuesta.json();
 
-    } catch (error) {
-        console.error(error)
-        mensaje.textContent = "Error al obtener todos los personajes.";
-    }
+    personajes = datos;
+    console.log(personajes);
+    renderizarPersonajes(personajes);
+  } catch (error) {
+    console.error(error);
+    mensaje.textContent = "Error al obtener todos los personajes.";
+  }
 }
 
 obtenerPersonajes();
 
 function renderizarPersonajes(personajes) {
-    contenedorPersonajes.innerHTML = "";
+  contenedorPersonajes.innerHTML = "";
 
-    personajes.forEach(personaje => {
+  personajes.forEach((personaje) => {
+    contenedorPersonajes.innerHTML += `
+            <div class="col-3 pb-2 d-flex justify-content-center">
+                <div class="card h-100">
+                
+                    <img 
+                    src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}"
+                    class="card-img-top"
+                    alt="${personaje.name}"
+                    >
 
-        contenedorPersonajes.innerHTML += `
-        
+                    <div class="card-body">
+
+                        <h5 class="card-tittle">${personaje.name}</h5>
+                        <p class="card-text">${personaje.ocupation}</p>
+                        <p class="card-text">${personaje.status}</p>
+
+                        <button 
+                            class="btn btn-warning" 
+                            data-id="${personaje.id}">
+                            Ver Detalle
+                        </button>
+
+                    </div>
+
+                </div>
+            </div>
         `;
-
-    });
+  });
 }
