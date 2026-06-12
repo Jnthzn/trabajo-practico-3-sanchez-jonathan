6e7d86ee224ddfe239bd5d1b5b22293b2a5e11e3
Jnthzn <sanchezbenja605@gmail.com>
2026-06-12 11:16:35 -0300

    feat: se implementó  modal de detalle de personajes

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,8 @@ const mensaje = document.getElementById("mensaje");
 const formBuscador = document.getElementById("formBuscador");
 const buscador = document.getElementById("buscador");
 
+const contenidoModal = document.getElementById("contenidoModal");
+
 async function obtenerPersonajes() {
   try {
     const respuesta = await fetch("https://thesimpsonsapi.com/api/characters");
@@ -109,7 +111,51 @@ async function obtenerDetallePersonaje(id) {
 
     const datos = await respuesta.json();
 
-    console.log(datos);
+    contenidoModal.innerHTML = `
+  <div class="row">
+    
+    <div class="col-4 text-center">
+      <img
+      src="https://cdn.thesimpsonsapi.com/500${datos.portrait_path}"
+      alt="${datos.name}"
+      class="img-fluid rounded"
+      >
+    </div>
+
+    <div class="col-8">
+      <h2>${datos.name}</h2>
+
+      <p><strong>Edad:</strong> ${datos.age}</p>
+
+      <p><strong>Fecha de nacimiento:</strong> ${datos.birthdate}</p>
+
+      <p><strong>Género:</strong> ${datos.gender}</p>
+
+      <p><strong>Ocupación:</strong> ${datos.occupation}</p>
+
+      <p><strong>Estado:</strong> ${datos.status}</p>
+
+      <hr>
+
+      <h5>Frase característica</h5>
+
+      <blockquote class="blockquote">
+        <p>
+          "${
+            datos.phrases.length > 0 ? datos.phrases[0] : "Sin frase disponible"
+          }"
+        </p>
+      </blockquote>
+
+    </div>
+
+  </div>
+
+  `;
+
+    const modal = new bootstrap.Modal(document.getElementById("modalDetalle"));
+
+    modal.show();
   } catch (error) {
     console.error(error);
   }
